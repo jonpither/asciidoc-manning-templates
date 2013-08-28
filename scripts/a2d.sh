86dc6769671f6d67f08af770d10571cd0f0555b7
Jon Pither <jon.pither@gmail.com>
2013-08-28 09:42:59 +0200

    templates dir

--- scripts/a2d.sh
+++ scripts/a2d.sh
@@ -3,7 +3,8 @@
 echo $BOOK_HOME
 
 MAKEPDF_HOME=$BOOK_HOME/AuthorInfo/AAMakePDF/
+TEMPLATES_HOME=$BOOK_HOME/asciidoc-manning-templates
 
-asciidoctor -T $BOOK_HOME/asciidoc/templates -a chapterFilename=$1 -b docbook45 -o $BOOK_HOME/manuscript/$1.xml $1.txt
-awk -f $BOOK_HOME/asciidoc/templates/scripts/cofix.awk $BOOK_HOME/manuscript/$1.xml > $BOOK_HOME/manuscript/$1.fixed
+asciidoctor -T $TEMPLATES_HOME -a chapterFilename=$1 -b docbook45 -o $BOOK_HOME/manuscript/$1.xml $1.txt
+awk -f $TEMPLATES_HOME/scripts/cofix.awk $BOOK_HOME/manuscript/$1.xml > $BOOK_HOME/manuscript/$1.fixed
 mv $BOOK_HOME/manuscript/$1.fixed $BOOK_HOME/manuscript/$1.xml
